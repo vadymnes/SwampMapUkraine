300db17b98178a385e89453cc7f5236711df8c82
Nesuk Vadym <vadymnesuk@gmail.com>
2025-02-02 20:07:22 +0200

    fix: links in accordion url

--- main.js
+++ main.js
@@ -146,7 +146,7 @@ fetch("https://raw.githubusercontent.com/vadymnes/SwampMapUkraine/main/swamp_pol
                     // Для кожного об'єкта додаємо назву та посилання в HTML-форматі
                     content += `
                         <p>
-                            <a style="display: inline-flex;  align-items: center;  gap: 8px;" href="${link.link}" target="_blank"><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="white"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-external-link"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6" /><path d="M11 13l9 -9" /><path d="M15 4h5v5" /></svg>${link.name}</a>
+                            <a style="display: inline-flex;  align-items: center;  gap: 8px;" href="${link.url}" target="_blank"><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="white"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-external-link"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6" /><path d="M11 13l9 -9" /><path d="M15 4h5v5" /></svg>${link.name}</a>
                         </p>
                     `;
                 });
